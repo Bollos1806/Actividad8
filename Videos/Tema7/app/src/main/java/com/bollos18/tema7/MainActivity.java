4387b75bb4effc5b55033e666097712417049466
Bollos1806 <issacedum_@hotmail.com>
2025-09-22 20:17:37 -0600

    Termino parteA

--- Videos/Tema7/app/src/main/java/com/bollos18/tema7/MainActivity.java
+++ Videos/Tema7/app/src/main/java/com/bollos18/tema7/MainActivity.java
@@ -1,24 +1,57 @@
-package com.bollos18.tema7;
+package com.bollos18.tema7; // ← cambia esto si es necesario
 
 import android.os.Bundle;
+import android.transition.Explode;
+import android.transition.Fade;
+import android.transition.Scene;
+import android.transition.Slide;
+import android.transition.Transition;
+import android.transition.TransitionManager;
+import android.transition.TransitionSet;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Scene sceneInitial;
+    private Scene sceneFinal;
+    private boolean showingInitial = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ViewGroup sceneRoot = findViewById(R.id.sceneRoot);
+
+        sceneInitial = Scene.getSceneForLayout(sceneRoot, R.layout.initial_layout, this);
+        sceneFinal   = Scene.getSceneForLayout(sceneRoot, R.layout.final_layout, this);
+
+        sceneInitial.enter();
+
+        Button btn = findViewById(R.id.btnToggle);
+        btn.setOnClickListener(v -> toggleScene());
+    }
+
+    private void toggleScene() {
+        Transition explode = new Explode();
+        Transition slide   = new Slide(Gravity.BOTTOM);
+        Transition fade    = new Fade();
+
+        TransitionSet set = new TransitionSet()
+                .addTransition(explode)
+                .addTransition(slide)
+                .addTransition(fade)
+                .setDuration(600);
+
+        if (showingInitial) {
+            TransitionManager.go(sceneFinal, set);
+        } else {
+            TransitionManager.go(sceneInitial, set);
+        }
+
+        showingInitial = !showingInitial;
     }
 }
